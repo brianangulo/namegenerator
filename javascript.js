dc5dd945db7be080f0ceeb7260551b928a06d27e
Brian Angulo <brianangulo96@gmail.com>
2021-10-31 01:54:24 -0500

    pedyo code

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const names=['will','bill','chill'];
+
+function getName(){
+    let guess="";
+    let correct=false;
+    
+    const guessy=Math.floor(math.random *(names.length));
+    const text= guessy[names.length];
+}
+
+
+
